c4a0d431c4c6187c25ab8f9d81f00e5dc27fe1ab
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-06-15 11:24:09 -0500

    feat: configured tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <> <Button variant="contained">Hello world</Button>
+      <div className='flex flex-col'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
